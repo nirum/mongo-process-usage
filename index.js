574d20360e318c0ecd5d96a809a0710379c850f0
Niru Maheswaranathan <nirum@stanford.edu>
2014-10-07 11:16:57 -0700

    v0.1: writes memory and cpu usage to database every second

---
+++ index.js
@@ -0,0 +1,57 @@
+console.log('-------------------------');
+console.log('-- Node System Monitor --');
+console.log('-------------------------\n');
+
+var exec = require('child_process').exec
+  , MongoClient = require('mongodb').MongoClient
+  , usage = require('usage')
+  , _ = require('lodash');
+
+// connect to database
+MongoClient.connect('mongodb://localhost:27017/usage', function(err, db) {
+
+  // function to log process
+  var logprocess = function(name, interval) {
+
+    // collection
+    var collection = db.collection(name);
+    
+    // get PIDs of process
+    var child = exec("ps -A | grep '[" + name[0] + "]" + name.substr(1) + "' | awk '{ print $1 }'", function(error, stdout, stderr) {
+
+      // split PIDs into an array, remove empty PIDs
+      ids = stdout.split("\n");
+      _.remove(ids, function(item) { return item.length == 0; });
+
+      // repeat every n milliseconds
+      setInterval(function() {
+
+        // for each PID
+        _.forEach(ids, function(pid) { 
+          usage.lookup(pid, function(err, result) {
+            collection.insert({
+              pid: pid,
+              name: name,
+              memory: result.memory,  // bytes
+              cpu: result.cpu,        // percentage
+              time: (new Date()).toISOString()
+            }, function(err, result) {
+              if (err !== null) {
+                console.log('Error saving to database!');
+                console.log(err);
+              }
+            });
+          });
+        });
+
+      }, interval);
+
+    });
+  }
+
+  // start logging some processes
+  logprocess('ipython', 1000);
+  logprocess('matlab', 1000);
+  console.log('Running process logs!')
+
+});
